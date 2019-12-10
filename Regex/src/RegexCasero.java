296aea02c30baab3e7a7fef20965b60b6db58fe8
oleksiiolefir <46476855+oleksiiolefir@users.noreply.github.com>
2019-12-10 08:14:43 +0100

    Add files via upload

---
+++ Regex/src/RegexCasero.java
@@ -0,0 +1,26 @@
+
+
+import java.util.Scanner;
+
+public class RegexCasero {
+
+	public void validarPorExpresion() {
+		String texto ="";
+		String expres = "";
+		Scanner reader = new Scanner(System.in);
+		//Mostrar las experesiones regulares que tenemos.
+		
+		
+		
+		
+		System.out.println("Introduzca texto ");
+		texto = reader.next();
+		System.out.println("Introduzca la expresion regular");
+		expres = reader.next();
+		
+		
+		
+		
+		
+	}
+}
