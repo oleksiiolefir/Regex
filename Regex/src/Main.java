3745d35f2619ea7466df669f13a1f246286a2b44
IN1DM3B_06 <IN1DM3B_06@H015112.NET>
2019-12-10 08:48:25 +0100

    iniciado la comprobacion de las letras

---
+++ Regex/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		RegexCasero reg = new RegexCasero();
+		reg.validarPorExpresion();
+	}
+
+}
--- Regex/src/RegexCasero.java
+++ Regex/src/RegexCasero.java
@@ -7,6 +7,7 @@ public class RegexCasero {
 	public void validarPorExpresion() {
 		String texto ="";
 		String expres = "";
+		boolean entrada = false;
 		Scanner reader = new Scanner(System.in);
 		//Mostrar las experesiones regulares que tenemos.
 		//[a,z] solo la 'a' y la 'z'
@@ -18,18 +19,28 @@ public class RegexCasero {
 		System.out.println("Introduzca la expresion regular");
 		expres = reader.next();
 		
+		
 		for(int i=0;i<texto.length();i++) 
 		{
 			char c = texto.charAt(i);
 			if(c!='a') {
-				
+				entrada = false;
+				break;
 			}
 			else if(c=='a') 
 			{
-				
+				entrada = true;
+
 			}
 		}
 		
+		if(entrada == true) {
+			System.out.println("El texto es correcto");
+		}
+		else {
+			System.out.println("El texto es incorrecto");
+		}
+			
 		
 		
 	}
