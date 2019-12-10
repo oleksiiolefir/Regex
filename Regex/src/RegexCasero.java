4b95166b0e25cfcb4468b54663d14e542be0c1db
jonbenja2 <jonbenja2@gmail.com>
2019-12-10 08:53:37 +0100

    proba

--- Regex/src/RegexCasero.java
+++ Regex/src/RegexCasero.java
@@ -33,4 +33,34 @@ public class RegexCasero {
 		
 		
 	}
+	public void validarPorNumber() {
+		String texto ="";
+		String expres = "";
+		Scanner reader = new Scanner(System.in);
+		//Mostrar las experesiones regulares que tenemos.
+		//[a,z] solo la 'a' y la 'z'
+		
+		
+		
+		System.out.println("Introduzca texto ");
+		texto = reader.next();
+		System.out.println("Introduzca la expresion regular");
+		expres = reader.next();
+		
+		for(int i=0;i<texto.length();i++) 
+		{
+			char c = texto.charAt(i);
+			if(c!='a') {
+				System.out.println("Introduzca la expresion ");
+			}
+			else if(c=='a') 
+			{System.out.println("Introduzca la expresion mal");
+				
+			}
+		}
+		
+		
+		
+	}
+	
 }
---
+++ Regex/src/principarl.java
@@ -0,0 +1,11 @@
+
+public class principarl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RegexCasero binario = new RegexCasero();
+		
+		binario.validarPorExpresion();
+	}
+
+}
