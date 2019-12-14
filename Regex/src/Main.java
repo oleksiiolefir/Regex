858de8504481e84baf6e5d1e90fc1c8f561a48c6
unknown <lander.arellanode@elorrieta-errekamari.com>
2019-12-14 18:30:30 +0100

    metodos nuevos regexCasero + descripcion

--- Regex/src/Main.java
+++ Regex/src/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		RegexCasero reg = new RegexCasero();
-		reg.validarPorExpresion();
+		{
+			RegexCasero reg = new RegexCasero();
+		
+				String texto ="";
+				String expres = "";
+				Scanner reader = new Scanner(System.in);
+				//Mostrar las experesiones regulares que tenemos.
+				//[a,z] solo la 'a' y la 'z'
+				
+				System.out.println("Expresiones regulares: \n  -Validar si esa letra se encuentra en el texto = , + letra + , + letra (hasta escribir todas las letras del texto))"
+						+ "\n  -Validar que la letra existe en el texto = ; + letra"
+						+ " \n  -Validar el minimo del texto = min + numero  "
+						+ " \n  -Validar si las letras estan entre las letras que estan entre - = letra - letra  ");
+				
+				System.out.println("Introduzca texto ");
+				texto = reader.nextLine().replaceAll(" ","");
+				System.out.println("Introduzca la expresion regular");
+				expres = reader.nextLine();
+				reg.validarPorExpresion(texto, expres);
+				reg.validarMinimo(texto, expres);
+				reg.validarPorComa(texto, expres);
+				reg.validarPorExpresionUnaLetra(texto, expres);
+			
+		}
+
 	}
 
 }
--- Regex/src/RegexCasero.java
+++ Regex/src/RegexCasero.java
@@ -1,12 +1,10 @@
 
-
 import java.util.Scanner;
 
 public class RegexCasero {
 
-	public void validarPorExpresion() {
-		String texto ="";
-		String expres = "";
+	public void validarPorExpresion(String texto, String expres) {
+		try {
 		boolean entrada = false;
 		Scanner reader = new Scanner(System.in);
 		//Mostrar las experesiones regulares que tenemos.
@@ -14,25 +12,57 @@ public class RegexCasero {
 		
 		
 		
-		System.out.println("Introduzca texto ");
-		texto = reader.next();
-		System.out.println("Introduzca la expresion regular");
-		expres = reader.next();
-		String[] parts = expres.split("");
-
+		
+		String[] parts1 = expres.split("");
+		String[] parts  = expres.split("");
 		for (int x=0;x<expres.length();x++) {
-			if (parts[x].equals("-")) {
+			if (parts1[x].equals("-")) {
 			  
+				
 			  int primero= parts[x-1].codePointAt(0);
 			  int ultimo= parts[x+1].codePointAt(0);
 			  int diferencia = ultimo - primero;
-				 parts =  new String[diferencia+1];
-			  for (int i=0;i<diferencia+1;i++) {
-				  
-				  parts[i] =(char)(primero+i)+"";
-				  System.out.println(parts[i]);
-				  System.out.println(diferencia+1);
-			  }
+			  parts =  new String[diferencia+1];
+				 if ((primero >= 65  && primero <= 90) || (primero >= 97  && primero <= 122) ) {
+					
+					 if ((ultimo >= 65  && ultimo <= 90) || (ultimo >= 97  && ultimo <= 122)) {
+						
+						 for (int i=0;i<diferencia+1;i++) {
+							  
+							  parts[i] =(char)(primero+i)+"";
+							  
+						  }
+						 
+					 }
+				 }
+				 
+				 for(int i=0;i<texto.length();i++) {
+						for(int a=0;a<parts.length;a++) {
+							
+						char c = texto.charAt(i);
+						char r = parts[a].charAt(0);
+						
+						if(c!=r) {
+							
+							entrada = false;
+							}
+						else if(c==r){
+							
+							entrada = true;
+							break;
+							}
+						}
+					if (entrada==false) {
+						break;
+					}
+					}
+				 if(entrada == true) {
+						System.out.println("El texto es correcto4");
+					}
+					else {
+						
+						System.out.println("El texto es incorrecto");
+					}
 
 			}
 			  
@@ -40,32 +70,102 @@ public class RegexCasero {
 		
 	
 			
-		for(int i=0;i<texto.length();i++) 
-		{
-			for(int a=0;a<parts.length;a++) {
-				
-			char c = texto.charAt(i);
-			char r = parts[a].charAt(0);
-			
-			if(c!=r) {
-				
-				entrada = false;
-				}
-			else if(c==r){
-				entrada = true;
-				break;
-				}
-			}
-		if (entrada=false) {
-			break;
+		
 		}
-		}
-		if(entrada == true) {
-			System.out.println("El texto es correcto");
-		}
-		else {
-			System.out.println("El texto es incorrecto");
+		catch(Exception e) {
 		}
 		
 	}
+	public void validarPorComa(String texto, String expres) {
+		try {
+			boolean entrada = false;		
+			String[] parts = expres.split(",");
+		
+				
+			for(int i=0;i<texto.length();i++) 
+			{
+				for(int a=1;a<=parts.length;a++) {
+					
+				char c = texto.charAt(i);
+				char r = parts[a].charAt(0);			
+				if(c!=r) {
+					
+					entrada = false;
+					}
+				else if(c==r){
+					entrada = true;
+					break;
+					}
+				}
+			
+			}
+			if(entrada == true) {
+				System.out.println("El texto es correcto3");
+			}
+			else {
+				System.out.println("El texto es incorrecto");
+			}
+		}catch(Exception e) {
+		}
+		
+	}
+	
+	public void validarPorExpresionUnaLetra(String texto, String expres) {
+		try {
+			boolean entrada = false;		
+			String[] parts = expres.split(";");
+		
+				
+			for(int a=1;a<=parts.length;a++) 
+			{ 
+				for(int i=0;i<texto.length();i++) {
+					
+				char c = texto.charAt(i);
+				char r = parts[a].charAt(0);			
+				if(c!=r) {
+					
+					entrada = false;
+					}
+				else if(c==r){
+					entrada = true;
+					a=parts.length;
+					break;
+					}
+				}
+			
+			}
+			if(entrada == true) {
+				System.out.println("El texto es correcto2");
+			}
+			else {
+				System.out.println("El texto es incorrecto");
+			}
+		}catch(Exception e) {
+		}
+		
+	}
+	
+	public void validarMinimo(String texto, String expres) { 
+		try {
+            boolean entrada = false;
+            String[] parts = expres.split("min");
+            
+            if(texto.length()<=Integer.parseInt(parts[1])) {
+                entrada = true;
+            }
+            else {
+                entrada = false;
+            }
+
+            if(entrada == true) {
+                System.out.println("El texto es correcto1");
+            }
+            else {
+                System.out.println("El texto es incorrecto");
+            }
+        }catch(Exception e) {
+        }
+
+	}
+	
 }
